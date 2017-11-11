9a9322b7b3a03648ed840064bb4fb7dcacfd574f
Katherine Belle Hartling <katbellehartling@gmail.com>
2017-11-11 02:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 // Code your solution in this file!
-var companyName = 'Scuber';
-const companyName = 'specialCompany';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith';
+let companyName = 'Scuber';
